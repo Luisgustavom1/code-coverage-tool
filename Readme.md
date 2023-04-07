8854e3f30e29e8f901b4fb0043e9b153f17e2d0e
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-04-07 20:20:25 -0300

    feat: coverage tool

---
+++ Readme.md
@@ -0,0 +1,6 @@
+https://www.youtube.com/watch?v=tDySiZck3jk
+
+https://nodejs.org/api/inspector.html
+https://v8.dev/docs/inspector
+https://chromedevtools.github.io/devtools-protocol/tot/Profiler/
+https://source.chromium.org/chromium/chromium/src/+/main:v8/test/debugger/test-api.js;l=1?q=test-api&ss=chromium
---
+++ coverage.mjs
@@ -0,0 +1,71 @@
+import inspector from 'inspector/promises';
+import { readFile } from 'node:fs/promises';
+import { isAbsolute } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const COLORS = {
+    GREEN: "\x1b[32m",
+    RED: "\x1b[31m",
+    END_LINE: "\x1b[0m"
+}
+
+const currentFileName = fileURLToPath(import.meta.url);
+
+const ENTRYPOINT = './example/index.mjs';
+const session = new inspector.Session();
+session.connect();
+
+await session.post("Profiler.enable");
+await session.post("Profiler.startPreciseCoverage", {
+    callCount: true,
+    detailed: true,
+});
+
+await import(ENTRYPOINT);
+
+const preciseCoverage = await session.post("Profiler.takePreciseCoverage");
+await session.post("Profiler.stopPreciseCoverage");
+
+const results = filterCoverageResults(preciseCoverage);
+for (const coverage of results) {
+    const filename = fileURLToPath(coverage.url);
+    const sourceCode = await readFile(filename, 'utf-8');
+    generateCoverageReport(
+        filename,
+        sourceCode,
+        coverage.functions,
+    );
+}
+
+function filterCoverageResults(coverage) {
+    return coverage.result.filter(({ url }) => {
+        const findUrl = url.replace("file://", "");
+        return isAbsolute(findUrl) && findUrl !== currentFileName;
+    })
+}
+
+function generateCoverageReport(filename, sourceCode, functions) {
+    const uncoveredLine = new Map();
+    
+    for (const func of functions) {
+        for (const range of func.ranges) {
+            if (range.count !== 0) continue;
+
+            const startLine = sourceCode.substring(0, range.startOffset).split("\n").length;
+            const endLine = sourceCode.substring(0, range.endOffset).split("\n").length;
+            for (let lineIndex = startLine; lineIndex <= endLine; lineIndex++) {
+                uncoveredLine.set(lineIndex, lineIndex);
+            }
+        }
+    }
+
+    console.log("\n" + COLORS.GREEN + filename + COLORS.END_LINE);
+    sourceCode.split('\n').forEach((line, index) => {
+        if (uncoveredLine.has(index + 1)) {
+            console.log(COLORS.RED + line + COLORS.END_LINE);
+            return;
+        };
+
+        console.log(line);
+    })
+}
